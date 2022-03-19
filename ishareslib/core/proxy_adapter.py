c231031c937fe716fefeb9b188371d16727f1c86
Fabian Jocks <dev@jocks.io>
2022-03-19 12:01:18 +0100

    feat(client): replaced obsolete proxy factory

---
+++ ishareslib/core/proxy_adapter.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+
+class Proxy:
+    def __init__(
+        self,
+        address: str,
+        port: int,
+        protocol: str = "http",
+        username: str = None,
+        password: str = None,
+    ):
+        self.address = address
+        self.port = port
+        self.protocol = protocol
+        self.username = username
+        self.password = password
+
+
+class ProxyAdapter(ABC):
+    def __init__(self):
+        self._proxy = None
+
+    def get_proxy(self) -> Proxy:
+        if self._proxy is None:
+            return self.new_proxy()
+        return self._proxy
+
+    @abstractmethod
+    def new_proxy(self) -> Proxy:
+        pass
