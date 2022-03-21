e2a818423dd7cb54f0ff7848994fb7581aca2198
Fabian Jocks <dev@jocks.io>
2022-03-21 13:40:46 +0100

    feat(client): added test cases for webshare_proxy_adapter.py

---
+++ tests/ext/proxy/test_webshare_proxy_adapter.py
@@ -0,0 +1,94 @@
+from typing import Union
+
+from faker import Faker
+from faker.providers import internet
+
+from ishareslib.ext.proxy.webshare_proxy_adapter import WebShareProxyAdapter
+
+
+def generate_list_mock(requests_mock, faker: Faker, pages: int = 1):
+    list_url = "proxy.webshare.io/api/proxy/list/?page=%d"
+
+    for i in range(pages):
+        previous_page: int = i
+        current_page: int = i + 1
+        next_page: int = i + 2
+
+        previous_page_url: Union[str, None] = None
+        current_page_url: str = "https://%s" % list_url % current_page
+        next_page_url: Union[str, None] = None
+
+        if previous_page > 0:
+            previous_page_url = list_url % previous_page
+
+        if next_page < pages:
+            next_page_url = list_url % next_page
+
+        requests_mock.get(
+            current_page_url,
+            json={
+                "count": pages,
+                "next": next_page_url,
+                "previous": previous_page_url,
+                "results": [
+                    {
+                        "username": faker.user_name(),
+                        "password": "jasdi2148",
+                        "proxy_address": faker.ipv4(),
+                        "ports": {
+                            "http": faker.port_number(),
+                            "socks5": faker.port_number(),
+                        },
+                        "valid": True,
+                        "last_verification": "2019-06-09T23:34:00.095501-07:00",
+                        "country_code": "US",
+                        "country_code_confidence": 0.95,
+                        "city_name": "New York",
+                    },
+                ],
+            },
+        )
+
+
+def test_new_proxy_with_single_response(requests_mock):
+    faker: Faker = Faker()
+    faker.add_provider(internet)
+
+    generate_list_mock(requests_mock, faker, pages=1)
+
+    adapter = WebShareProxyAdapter("")
+    proxy = adapter.get_proxy()
+    assert proxy.address == "101.135.106.135"
+    assert proxy.port == 8144
+    assert proxy.username == "alex38"
+
+
+def test_new_proxy_with_multiple_responses(requests_mock):
+    faker: Faker = Faker()
+    faker.add_provider(internet)
+
+    generate_list_mock(requests_mock, faker, pages=2)
+
+    adapter = WebShareProxyAdapter("")
+    for i in range(1):
+        proxy = adapter.new_proxy()
+        if proxy.address == "38.11.179.14":
+            assert proxy.port == 57336
+            assert proxy.username == "jjackson"
+        else:
+            assert proxy.address == "101.135.106.135"
+            assert proxy.port == 8144
+            assert proxy.username == "alex38"
+
+
+def test_new_proxy_with_fixed_target_page_response(requests_mock):
+    faker: Faker = Faker()
+    faker.add_provider(internet)
+
+    generate_list_mock(requests_mock, faker, pages=2)
+
+    adapter = WebShareProxyAdapter("", page=2)
+    proxy = adapter.new_proxy()
+    assert proxy.address == "65.198.154.34"
+    assert proxy.port == 12881
+    assert proxy.username == "jonathanwelch"
