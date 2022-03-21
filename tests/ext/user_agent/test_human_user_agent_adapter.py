35314d260c9a2060680c9686ef3bb625c4c0bcd9
Fabian Jocks <dev@jocks.io>
2022-03-21 11:34:33 +0100

    feat(client): renamed factory variable to adapter in test_human_user_agent_adapter.py

--- tests/ext/user_agent/test_human_user_agent_adapter.py
+++ tests/ext/user_agent/test_human_user_agent_adapter.py
@@ -2,6 +2,6 @@ from ishareslib.ext.user_agent.human_user_agent_adapter import HumanUserAgentAda
 
 
 def test_get_user_agent():
-    factory = HumanUserAgentAdapter()
-    user_agent = factory.get_user_agent()
+    adapter = HumanUserAgentAdapter()
+    user_agent = adapter.get_user_agent()
     assert user_agent.__contains__("Mozilla")
