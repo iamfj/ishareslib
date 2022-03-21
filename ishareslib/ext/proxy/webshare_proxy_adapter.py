40a0e7ded210441ca9e7ed97716312c7469702de
Fabian Jocks <dev@jocks.io>
2022-03-21 12:27:32 +0100

    feat(client): added host as parameter to webshare_proxy_adapter.py

--- ishareslib/ext/proxy/webshare_proxy_adapter.py
+++ ishareslib/ext/proxy/webshare_proxy_adapter.py
@@ -7,11 +7,16 @@ from ishareslib.core.proxy_adapter import Proxy, ProxyAdapter
 
 
 class WebShareProxyAdapter(ProxyAdapter):
-    def __init__(self, key: str, page: Union[int, None] = None):
+    def __init__(
+        self,
+        key: str,
+        page: Union[int, None] = None,
+        host: str = "proxy.webshare.io/api",
+    ):
         super().__init__()
         self._key: str = key
         self._page: Union[int, None] = page
-        self._host: str = "https://proxy.webshare.io/api"
+        self._host: str = host
         self._proxies: list[Proxy] = []
 
     def new_proxy(self) -> Proxy:
@@ -21,7 +26,7 @@ class WebShareProxyAdapter(ProxyAdapter):
     def _before_new_proxy(self) -> None:
         if len(self._proxies) == 0:
             self._get_proxy_list(
-                "%s/proxy/list/?page=%d" % (self._host, self._page or 1)
+                "https://%s/proxy/list/?page=%d" % (self._host, self._page or 1)
             )
 
     def _gen_proxy(self) -> Union[Proxy, None]:
@@ -41,4 +46,4 @@ class WebShareProxyAdapter(ProxyAdapter):
                 )
 
         if self._page is None and response["next"] is not None:
-            self._get_proxy_list(response["next"])
+            self._get_proxy_list("https://%s" % response["next"])
